8a2c6158adeb44fdd4de9225bdee16f653689911
Daps <52228501+daps10@users.noreply.github.com>
2022-10-13 16:08:19 +0530

    Added validators in the signup field.

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator');
 
 const {
     signup,
@@ -8,7 +9,28 @@ const {
 } = require("../controllers/authController");
 
 // signout
-router.post('/signup', signup)
+router.post(
+    '/signup',
+    [
+        check('name', "Name field is required")
+            .isLength({ min: 3 })
+            .withMessage('Name should be at least 3 chars long')
+            .isLength({ min: 32 })
+            .withMessage('Name should be at least 32 chars long'),
+        check('lastName', "Last name field must be required!")
+            .isLength({ min: 3 })
+            .withMessage('Last name should be at least 3 chars long')
+            .isLength({ min: 32 })
+            .withMessage('Last name should be at least 32 chars long'),
+        check('email', "Email should be atleast 5 characters").isEmail(),
+        check('password', "Password field must be required!")
+            .isLength({ min: 3 })
+            .withMessage('Name should be at least 3 chars long')
+            .isLength({ min: 16 })
+            .withMessage('Password should be at least 16 chars long'),
+    ], 
+    signup
+)
 
 // signin
 router.post('/signin', signin)
