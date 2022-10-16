57efcedf56ae35f45942c7b317953fc322c91d8f
Daps <52228501+daps10@users.noreply.github.com>
2022-10-16 14:54:51 +0530

    Given validations for the product module.

---
+++ projbackend/validations/product.validation.js
@@ -0,0 +1,50 @@
+const { check, validationResult } = require('express-validator');
+
+const addProduct = [
+    check('name', "Name field is required")
+        .not().isEmpty()
+        .withMessage('Name should not be empty')    
+        .isLength({ min: 3 })
+        .withMessage('Name should be at least 3 chars long')
+        .isLength({ max: 32 })
+        .withMessage('Name should not be more than 32 chars long'),
+    check('description', "Description field is required")
+        .not().isEmpty()
+        .withMessage('Description should not be empty')    
+        .isLength({ min: 3 })
+        .withMessage('Description should be at least 3 chars long')
+        .isLength({ max: 2000 })
+        .withMessage('Description should not be more than 2000 chars long'),
+    check('price', "Price field is required")
+        .not().isEmpty()
+        .withMessage('Price should not be empty')    
+        .isDecimal()
+        .withMessage('Price must be numeric.')
+];
+
+const updateProduct = [
+    check('name', "Name field is required")
+        .not().isEmpty()
+        .withMessage('Name should not be empty')    
+        .isLength({ min: 3 })
+        .withMessage('Name should be at least 3 chars long')
+        .isLength({ max: 32 })
+        .withMessage('Name should not be more than 32 chars long'),
+    check('description', "Description field is required")
+        .not().isEmpty()
+        .withMessage('Description should not be empty')    
+        .isLength({ min: 3 })
+        .withMessage('Description should be at least 3 chars long')
+        .isLength({ max: 2000 })
+        .withMessage('Description should not be more than 2000 chars long'),
+    check('price', "Price field is required")
+        .not().isEmpty()
+        .withMessage('Price should not be empty')    
+        .isDecimal()
+        .withMessage('Price must be numeric.')
+];
+
+module.exports = {
+    addProduct,
+    updateProduct
+};
