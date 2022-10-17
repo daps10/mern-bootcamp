ace2a2e27985ade35a014983e4f0d165e8979d08
Daps <52228501+daps10@users.noreply.github.com>
2022-10-17 16:22:04 +0530

    Completed get order API.

--- projbackend/controllers/order.controller.js
+++ projbackend/controllers/order.controller.js
@@ -9,9 +9,28 @@ exports.getAllOrders = async (req, res) => {
 };
 
 exports.getOrder = async (req, res) => {
-    res.status(200).json({ 
-        msg: 'Get order details successfully' 
-    });
+    try {
+        const orderData = req.orderData;
+        
+        if(!orderData) {
+            return res.status(httpStatus.NOT_FOUND).json({
+                status: httpStatus.NOT_FOUND,
+                message:  t("text_order_not_found"),
+            });
+        } 
+
+        res.status(httpStatus.OK).json({
+            status: httpStatus.OK,
+            message: t("text_order_found"),
+            response: orderData
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            status: httpStatus.INTERNAL_SERVER_ERROR,
+            message: t("text_rest_something_went_wrong")
+        });     
+    }
 };
 
 exports.createOrder = async (req, res) => {
