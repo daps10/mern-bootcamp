31be07d5e6b283a98b4b9c2e203cbba4a2a7262a
Daps <52228501+daps10@users.noreply.github.com>
2022-10-18 23:33:53 +0530

    Setup the css and javascript links.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles.css"
 
 const Home = () => {
     return (
         <div>
-            <h1>Home is available</h1>
+            <h1 className='text-white'>Home is available</h1>
         </div>
     )
 }
--- projfrontend/src/index.js
+++ projfrontend/src/index.js
@@ -3,3 +3,4 @@ import Routes from "./Routes";
 import ReactDOM  from "react-dom";
 
 ReactDOM.render(<Routes/>, document.getElementById("root"));
+
