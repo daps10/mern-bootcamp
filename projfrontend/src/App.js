7bb176076653e31383115d87b0cddeffbe6b5796
Daps <52228501+daps10@users.noreply.github.com>
2022-10-18 22:59:06 +0530

    Launch create-react-app command to setup file

---
+++ projfrontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          React APP working
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
