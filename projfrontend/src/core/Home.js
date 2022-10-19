60ed6713d3f76fafc7bbebed47a6632276e5562e
Daps <52228501+daps10@users.noreply.github.com>
2022-10-19 20:32:10 +0530

    Created base container which can consider the footer and nav bar for throughout the application.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import "../styles.css"
 import { API } from "../backend";
+import Base from './Base';
 
 const Home = () => {
     console.log("API IS ", API)
     return (
-        <div>
+        <Base title='Home Page' description='My description'>
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn-success">Test</button>
+                </div>
+            </div>
             <h1 className='text-white'>Home is available</h1>
-        </div>
+        </Base>
     )
 }
 
