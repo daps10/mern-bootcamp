96dc9703d089c7399b1780f21ec1f7f1bffc25ff
Daps <52228501+daps10@users.noreply.github.com>
2022-10-20 18:44:32 +0530

    Added signin logic for the authentication.

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, Redirect } from 'react-router-dom';
 import Base from '../core/Base';
+import { signin, authenticate, isAuthenticated } from "../auth/helper/index"; 
 
 const Signin = () => {
+    const [values, setValues] = useState({
+        email:"",
+        password:"",
+        error: "",
+        loading: false,
+        didRedirect: false
+    });
+
+    // destructure 
+    const [email, password, error, loading, didRedirect] = values;
+    const { user } = isAuthenticated();
+
+    // Onchange method
+    const onHandleChange = name => event => {
+        setValues({
+            ...values,
+            error:false,
+            [name]: event.target.value
+        })
+    }
+
+    const successMessage = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div className="alert alert-success" style={{display: success? "": "none"}}>
+                        New Account was created successfully. Please <Link to="/signin"> Login Here </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const errorMessage = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div className="alert alert-danger" style={{display: error ? "": "none"}}>
+                        { error }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const onSubmit = async(event) => {
+        event.preventDefault();
+
+        setValues({
+            ...values,
+            error:false,
+            loading: true
+        });
+
+        const response = await signup({
+            email,
+            password
+        });
+        console.log("response found === ", response);
+        if(response.status !== 200){
+            setValues({ 
+                ...values, 
+                error: response.message, 
+                loading: false 
+            });
+        } else {
+            setValues({ 
+                ...values, 
+                email:"",
+                password:"",
+                error: "", 
+                loading: false,
+                didRedirect:true
+            });
+        }
+    }
+    
     const signInForm = () => {
         return (
             <div className="row">
@@ -9,13 +88,13 @@ const Signin = () => {
                     <form >
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" type="email"/>
+                            <input className="form-control" type="email" value={email} onChange={ onHandleChange }/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" type="password"/>
+                            <input className="form-control" type="password" value={password} onChange={ onHandleChange }/>
                         </div>
-                        <button className="form-control btn btn-success btn-block mt-2">
+                        <button className="form-control btn btn-success btn-block mt-2" onClick={onSubmit}>
                             Submit
                         </button>
                     </form>
@@ -27,6 +106,8 @@ const Signin = () => {
     return (
     <>
         <Base title="Sign in page" description="A page for user to signin">
+            { successMessage() }
+            { errorMessage() }
             { signInForm() }
         </Base>
     </>
