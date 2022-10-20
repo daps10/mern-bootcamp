550877ec4301ff097da1ab7f0b081fcfbf7db364
Daps <52228501+daps10@users.noreply.github.com>
2022-10-20 15:31:40 +0530

    Signup and signin form

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -2,13 +2,35 @@ import React from 'react'
 import Base from '../core/Base';
 
 const Signin = () => {
-  return (
+    const signInForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password"/>
+                        </div>
+                        <button className="form-control btn btn-success btn-block mt-2">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+    return (
     <>
         <Base title="Sign in page" description="A page for user to signin">
-            <div>Signin</div>
+            { signInForm() }
         </Base>
     </>
-  )
+    )
 }
 
 export default Signin;
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -2,10 +2,37 @@ import React from "react";
 import Base from "../core/Base";
 
 const Signup = () => {
+
+    const signUpForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form >
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input className="form-control" type="text"/>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password"/>
+                        </div>
+                        <button className="form-control btn btn-success btn-block mt-2">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <Base title="Sign up page" description="A page for user to signup">
-                <h1>Signup Page</h1>
+                { signUpForm() }
             </Base>
         </>
     )
