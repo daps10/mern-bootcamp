956757b1e9f6083d3f7be6dd67c394b145556b20
Daps <52228501+daps10@users.noreply.github.com>
2022-10-23 00:56:29 +0530

    Called the create category API.

---
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -0,0 +1,26 @@
+import { API } from "../../backend";
+
+export const createCategory = async ( token, categoryName ) => {
+    try {
+        const categoryParams = {
+            "name": categoryName
+        };
+        
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                "authorization" : "Bearer " + token
+            },
+            body: JSON.stringify( categoryParams )
+        };
+
+        const URL =  API + "category/create";
+            
+        const response = await fetch (URL, requestOptions);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
