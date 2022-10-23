850aa6255a142889d246c986eab3e8ce8b3ae722
Daps <52228501+daps10@users.noreply.github.com>
2022-10-23 19:16:53 +0530

    Created APIs for get all and single product.

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -51,7 +51,49 @@ export const getAllCategories = async() => {
     }
 }
 
+// Get all products
+export const getAllProducts = async() => {
+    try {
+        const token = isAuthenticated();
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json',
+                "authorization" : "Bearer " + token
+            }
+        };
 
+        const URL =  API + "product/";
+            
+        const response = await fetch (URL, requestOptions);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Get Product
+export const getProduct = async(productId) => {
+    try {
+        const token = isAuthenticated();
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json',
+                "authorization" : "Bearer " + token
+            }
+        };
+
+        const URL =  API + "product/" + productId;
+            
+        const response = await fetch (URL, requestOptions);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // create product
 export const createProduct = async ( productParams ) => {
