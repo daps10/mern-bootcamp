557f9fcc1e08d1c1bab7d1bd9d2d2bf29fc40fb7
Daps <52228501+daps10@users.noreply.github.com>
2022-10-27 23:19:52 +0530

    remove the uselocation which was not used,

--- projfrontend/src/admin/UpdateProduct.js
+++ projfrontend/src/admin/UpdateProduct.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useLocation, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Base from '../core/Base';
 import { 
     getAllCategories, 
