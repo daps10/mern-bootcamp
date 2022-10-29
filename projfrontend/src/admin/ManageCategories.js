d074aa71b166862ba5a92f024995ce635a12e002
Daps <52228501+daps10@users.noreply.github.com>
2022-10-30 00:30:30 +0530

    Resolved all the issues for get all categories

--- projfrontend/src/admin/ManageCategories.js
+++ projfrontend/src/admin/ManageCategories.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import Base from '../core/Base';
+import { getAllCategories } from './helper/adminapicall';
 
 const ManageCategories = () => {
+    // run the preload function to fetch all the categories
+    useEffect(() => {
+      preload();
+    }, []);
+
+    const [categories, setCategories] = useState([]);
+    
+    // Fetch all categories
+    const preload = async () => {
+        const response = await getAllCategories();
+        if(response.status === 200){
+            setCategories(response.response);
+        } else {
+            console.log("error data :: ", response.message);
+        }    
+    }
+
+
     return (
         <Base title="Welcome admin" description="Manage products here">
             <h2 className="mb-4">All products:</h2>
@@ -10,9 +30,9 @@ const ManageCategories = () => {
             </Link>
             <div className="row">
                 <div className="col-12">
-                    <h2 className="text-center text-white my-3">Total 3 products</h2>
+                    <h2 className="text-center text-white my-3">Total categories</h2>
 
-                    { products.map((res, index) => {
+                    { categories.map((res, index) => {
                         return  (
                             <div key={ index } className="row text-center mb-2 ">
                                 <div className="col-4">
@@ -21,18 +41,18 @@ const ManageCategories = () => {
                                 <div className="col-4">
                                     <Link
                                         className="btn btn-success"
-                                        to={`/admin/product/${res._id}`}
+                                        to={`/admin/category/${res._id}`}
                                     >
                                         <span className="">Update</span>
                                     </Link>
                                 </div>
-                                <div className="col-4">
+                                {/* <div className="col-4">
                                     <button onClick={() => {  
                                         deleteThisProduct(res._id);
                                     }} className="btn btn-danger">
                                         Delete
                                     </button>
-                                </div>
+                                </div> */}
                             </div>
                         )
                     }) }
