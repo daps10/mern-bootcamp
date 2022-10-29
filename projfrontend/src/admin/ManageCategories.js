c0d9f09f8de195205b4857304f0ec1fa898e58e3
Daps <52228501+daps10@users.noreply.github.com>
2022-10-30 00:27:11 +0530

    Listout all the categories

--- projfrontend/src/admin/ManageCategories.js
+++ projfrontend/src/admin/ManageCategories.js
@@ -3,8 +3,42 @@ import Base from '../core/Base';
 
 const ManageCategories = () => {
     return (
-        <Base>
-            <h1 className="text-white">TODO for the assigments</h1>
+        <Base title="Welcome admin" description="Manage products here">
+            <h2 className="mb-4">All products:</h2>
+            <Link className="btn btn-info" to={`/admin/dashboard`}>
+                <span className="">Admin Home</span>
+            </Link>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="text-center text-white my-3">Total 3 products</h2>
+
+                    { products.map((res, index) => {
+                        return  (
+                            <div key={ index } className="row text-center mb-2 ">
+                                <div className="col-4">
+                                    <h3 className="text-white text-left">{ res.name }</h3>
+                                </div>
+                                <div className="col-4">
+                                    <Link
+                                        className="btn btn-success"
+                                        to={`/admin/product/${res._id}`}
+                                    >
+                                        <span className="">Update</span>
+                                    </Link>
+                                </div>
+                                <div className="col-4">
+                                    <button onClick={() => {  
+                                        deleteThisProduct(res._id);
+                                    }} className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    }) }
+
+                </div>
+            </div>
         </Base>
     )
 }
