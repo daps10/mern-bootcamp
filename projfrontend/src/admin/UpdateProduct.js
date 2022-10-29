1360735f9c820b6a087a3975ca789bd06f5ae241
Daps <52228501+daps10@users.noreply.github.com>
2022-10-29 23:25:40 +0530

    changed the title

--- projfrontend/src/admin/UpdateProduct.js
+++ projfrontend/src/admin/UpdateProduct.js
@@ -241,7 +241,7 @@ const UpdateProduct = (props) => {
                 onClick={onSubmit}
                 className="btn btn-outline-success mb-3"
             >
-                Update Product
+                Update a Product
             </button>
         </form>
     );
