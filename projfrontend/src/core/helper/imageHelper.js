a98cfae93bdfee8443971a2d4d94cd17139c6b56
Daps <52228501+daps10@users.noreply.github.com>
2022-11-01 22:32:18 +0530

    changed max height back to the 100.

--- projfrontend/src/core/helper/imageHelper.js
+++ projfrontend/src/core/helper/imageHelper.js
@@ -6,7 +6,7 @@ const ImageHelper = ({ product }) => {
             <img
               src="https://images.unsplash.com/photo-1597576000003-2e6487e67d20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
               alt="photo"
-              style={{ maxHeight: "59%", maxWidth: "100%" }}
+              style={{ maxHeight: "100%", maxWidth: "100%" }}
               className="mb-3 rounded"
             />
         </div>
