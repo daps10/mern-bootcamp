6386fe9e9768323d217a7ee7f0738e8db6972964
Daps <52228501+daps10@users.noreply.github.com>
2022-11-05 00:17:27 +0530

    Called the imageurl as static one with ternary operator

--- projfrontend/src/core/helper/imageHelper.js
+++ projfrontend/src/core/helper/imageHelper.js
@@ -3,11 +3,12 @@ import { API } from '../../backend';
 
 
 const ImageHelper = ({ product }) => {
+    let imageUrl = product ? `${API}/product/photo/${product.id}` : "https://images.unsplash.com/photo-1597576000003-2e6487e67d20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
     return (
         <div className="rounded border border-success p-2">
             <img
               src={
-                `${API}/product/photo/${product.id}`
+                imageUrl
               }
               alt="photo"
               style={
