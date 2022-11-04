550900ef525de0a04cbdeeacc1ef2e26da795ede
Daps <52228501+daps10@users.noreply.github.com>
2022-11-05 00:46:47 +0530

    Listout all the products with the setproducts hook.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "../styles.css"
 import Base from './Base';
 import Card from './card';
-
+import { getAllProducts } from './helper/coreapicalls';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProducts = async () => {
+        const response = await getAllProducts();
+        if(response.status === 200){
+            setProducts(response.response);
+        } else {
+            console.log("error data :: ", response.message);
+            setError(response.message);
+        }
+    }
+
+    useEffect(() => {
+      loadAllProducts()
+      // eslint-disable-next-line
+    }, []);
+
     return (
         <>
         <Base title='Home Page' description='My description'>
             <div className="row text-center">
-                <div className="col-4">
-                    <Card/>
+                <h1 className="text-white">All of tshirts</h1>
+
+                <div className="row">
+                    {
+                        products.map((product, index) => {
+                            return (
+                                <div key={index} className="col-4 mb-4">
+                                    <Card />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-                <div className="col-4">
-                    <Card/>
-                </div>
-                <div className="col-4">
-                    <Card/>
-                </div>
+
             </div>
         </Base>
         </>
